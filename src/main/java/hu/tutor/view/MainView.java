246895ba9b59ce4796d2b64a6a346179e87903bc
Szabó Dániel <szabo.daniel88@gmail.com>
2018-11-03 15:50:09 +0100

    welcome message

--- src/main/java/hu/tutor/view/MainView.java
+++ src/main/java/hu/tutor/view/MainView.java
@@ -4,11 +4,14 @@ import com.vaadin.navigator.View;
 import com.vaadin.navigator.ViewChangeListener.ViewChangeEvent;
 import com.vaadin.server.ExternalResource;
 import com.vaadin.spring.annotation.SpringView;
+import com.vaadin.ui.Alignment;
 import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
 import com.vaadin.ui.Link;
+import com.vaadin.ui.VerticalLayout;
 
 @SpringView(name = MainView.MAIN_VIEW_NAME)
-public class MainView extends HorizontalLayout implements View {
+public class MainView extends VerticalLayout implements View {
 
 	private static final long serialVersionUID = -891813713655196146L;
 
@@ -20,7 +23,11 @@ public class MainView extends HorizontalLayout implements View {
 		Link loginLink = new Link("Bejelentkezés", new ExternalResource("#!" + LoginView.LOGIN_VIEW_NAME));
 		Link searchLink = new Link("Keresés", new ExternalResource("#!" + SearchView.SEARCH_VIEW_NAME));
 
-		this.addComponents(registerLink, loginLink, searchLink);
+		Label welcomeLabel = new Label("Üdvözöljük a magánoktatók apróhirdetési weboldalán!");
+		welcomeLabel.addStyleName("h1");
+
+		this.addComponents(new HorizontalLayout(registerLink, loginLink, searchLink), welcomeLabel);
+		this.setComponentAlignment(welcomeLabel, Alignment.MIDDLE_CENTER);
 		this.setMargin(true);
 		this.addStyleName("tutor-background");
 
